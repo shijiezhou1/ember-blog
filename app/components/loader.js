c5ec9c4f07027c77fbed236fa71f184d58b924d5
Shijie Zhou <cs5129606@gmail.com>
2019-06-20 22:26:37 -0600

    adding loader and header

---
+++ app/components/loader.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  loadingQuery: computed('loadingQuery', function () {
+    console.log('ok')
+  })
+});
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,8 +2,27 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   actions: {
-    flatButton: function () {
-      console.log('print ');
+    HOME: function () {
+      console.log('called');
+    },
+    SELF: function () {
+      console.log('called');
+    },
+    TECH: function () {
+      console.log('called');
+    },
+    HEATED: function () {
+      console.log('called');
+    },
+    DESIGN: function () {
+      console.log('called');
+    },
+    CULTURE: function () {
+      console.log('called');
+    },
+    MORE: function () {
+      console.log('called');
     }
+
   }
 });
