900a2219905a5ed9484707e490b40f889d30a98e
Shijie Zhou <cs5129606@gmail.com>
2019-06-30 12:53:29 -0600

    form is working and capturing

---
+++ app/controllers/tech.js
@@ -0,0 +1,36 @@
+import Controller from '@ember/controller';
+import { A } from '@ember/array';
+import { observer, computed } from "@ember/object";
+
+/**
+ * The controller for the tech page
+ *
+ * @module
+ * @uses ember/controller
+ * @namespace Controller
+ * @class tech
+ */
+export default Controller.extend({
+  pizzSizeResult: observer('pizzaSize', function () {
+    return this.get('pizzaSize');
+  }),
+  sizes: A([
+    1, 2, 3, 4
+  ]),
+  dynamicItem: observer('pizzaSize', function () {
+    let json = [];
+    let length = this.get('pizzaSize');
+    let items = [];
+    for (var i = 1; i <= length; i++) {
+      items.push({ name: 'name-' + i, value: json, address: '' })
+    }
+    // set the item on each observer
+    this.set('formLists', items);
+  }),
+
+  actions: {
+    flatButton() {
+      console.log(this.get('formLists'));
+    }
+  }
+});
--- app/routes/tech.js
+++ app/routes/tech.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
+/**
+ * The route for the tech page
+ *
+ * @module
+ * @namespace Route
+ * @class tech
+ * @extends ember/routing/route
+ */
 export default Route.extend({
+  init(){
+    this._super(...arguments);
+  },
 });
