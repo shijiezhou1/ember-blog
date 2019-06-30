a68745fce1b94ff2a2187a06ae42e32a6ff0b95f
Shijie Zhou <cs5129606@gmail.com>
2019-06-30 13:53:29 -0600

    remove unuse component

--- app/controllers/tech.js
+++ app/controllers/tech.js
@@ -11,15 +11,15 @@ import { observer, computed } from "@ember/object";
  * @class tech
  */
 export default Controller.extend({
-  pizzSizeResult: computed('pizzaSize', function () {
-    return this.get('pizzaSize');
+  pizzSizeResult: computed('formSize', function () {
+    return this.get('formSize');
   }),
   sizes: A([
     1, 2, 3, 4
   ]),
-  dynamicItem: observer('pizzaSize', function () {
+  dynamicItem: observer('formSize', function () {
     let json = [];
-    let length = this.get('pizzaSize');
+    let length = this.get('formSize');
     let items = [];
     for (var i = 1; i <= length; i++) {
       items.push({ name: 'name-' + i, value: json, address: '' })
