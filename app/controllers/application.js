806f05d142d8be0980ffb06b71786e4fde391c49
Shijie Zhou <cs5129606@gmail.com>
2019-07-02 08:32:53 -0600

    temperate work

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,27 +5,5 @@ export default Controller.extend({
   setup() {
     // console.log(model)
   },
-  actions: {
-    HOME: function () {
-      this.transitionToRoute('home');
-    },
-    SELF: function () {
-      this.transitionToRoute( 'self' );
-    },
-    TECH: function () {
-      this.transitionToRoute( 'tech' );
-    },
-    HEATED: function () {
-      this.transitionToRoute( 'heated' );
-    },
-    DESIGN: function () {
-      this.transitionToRoute( 'design' );
-    },
-    CULTURE: function () {
-      this.transitionToRoute( 'culture' );
-    },
-    MORE: function () {
-      this.transitionToRoute( 'more' );
-    }
-  }
+
 });
