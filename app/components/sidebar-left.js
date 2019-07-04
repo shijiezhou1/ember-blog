1abddb88e843cdfe7ca5833e890671e5c9c03f44
Shijie Zhou <cs5129606@gmail.com>
2019-07-04 10:42:51 -0600

    ember array

--- app/components/sidebar-left.js
+++ app/components/sidebar-left.js
@@ -1,22 +1,34 @@
 import Component from '@ember/component';
 import {computed} from '@ember/object';
+import {A} from '@ember/array';
 
 export default Component.extend({
   classNames: ['application_float_card_left'],
   numOfRows: 3,
-
+  listInfo: A(['javascript', 'typescript', 'vue']),
   listData: computed('numOfRows', function () {
     let contacts = [];
     let numOfRows = this.get('numOfRows');
 
     for (let i = 0; i < numOfRows; i++) {
       contacts.push({
-        name: 'fake name ' + i,
-        email: 'fake email ' + i,
+        name: this.listInfo[i],
+        email: 'www.g@mail.com',
         img: 'http://lorempixel.com/50/50/people?0'
       });
     }
 
     return contacts;
   }),
+  actions: {
+    javascript() {
+      console.log('ok - javascript');
+    },
+    typescript() {
+      console.log('ok - typescript');
+    },
+    vue() {
+      console.log('ok - vue');
+    }
+  }
 });
