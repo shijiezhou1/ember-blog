e79db19531d6acb939cfefe0a6bc7630f831250f
Shijie Zhou <cs5129606@gmail.com>
2019-07-11 18:47:43 -0600

    asdsa

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 
 /**
  * the basic page for home
- *
  * @class home
  * @uses ember/routing/route
  * @namespace Route
@@ -11,12 +10,13 @@ import Route from '@ember/routing/route';
  */
 export default Route.extend({
   model() {
-    let homePost = this.get('store').find('home');
-    console.log('typeof home :', typeof homePost);
-    homePost.then((res)=> {
-      console.dir(res)
-    })
-    return homePost;
+    return this.store.findAll('home');
+    // let homePost = this.get('store').find('home');
+    // console.log('typeof home :', typeof homePost);
+    // homePost.then((res)=> {
+    //   console.dir(res)
+    // })
+    // return homePost;
     // return this.store.findRecord('home', params.home_id);
   }
 });
