ec771714d8fa1ed9fa66403b098c7da96e689919
Shijie Zhou <cs5129606@gmail.com>
2019-07-24 15:56:24 -0600

    starting mirage on deployment. let's checkit out

--- config/environment.js
+++ config/environment.js
@@ -23,6 +23,8 @@ module.exports = function(environment) {
     }
   };
 
+  ENV['ember-cli-mirage'] = { enabled: true, autostart: true };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
