777761f89f92f518169ddbd596a4a44daf5ca6b3
Shijie Zhou <cs5129606@gmail.com>
2019-07-25 22:05:24 -0600

    adding one more task and remind my self with the next goal.

--- app/components/article-list.js
+++ app/components/article-list.js
@@ -1,5 +1,10 @@
 import Component from '@ember/component';
 
-export default Component.extend( {
-  tagName: ""
-} );
+export default Component.extend({
+  tagName: "",
+  actions: {
+    goToPost(id){
+      console.log('this is not right' + id);
+    }
+  }
+});
