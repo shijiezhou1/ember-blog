02fff413da3ff1d5bc101926c70f447b2c541a21
Shijie Zhou <cs5129606@gmail.com>
2019-07-25 22:05:24 -0600

    nice and awesome code

--- app/components/article-list.js
+++ app/components/article-list.js
@@ -1,5 +1,13 @@
 import Component from '@ember/component';
 
-export default Component.extend( {
-  tagName: ""
-} );
+export default Component.extend({
+  tagName: "",
+  actions: {
+    goToPost(id){
+      console.log('this is not right' + id);
+    },
+    wow(){
+      console.log( 'wow is called' )
+    }
+  }
+});
--- app/models/post.js
+++ app/models/post.js
@@ -5,6 +5,7 @@ export default Model.extend({
   title: DS.attr('string'),
   text: DS.attr('string'),
   img: DS.attr('string'),
+  url: DS.attr('string'),
   iconButtonList: DS.attr(),
   categoryList: DS.attr(),
   viewCount: DS.attr()
