1ca6ad1033975bebd9a5ceb530a5b40190c92e18
Shijie Zhou <cs5129606@gmail.com>
2019-07-26 20:23:17 -0600

    Ready for the next update

--- app/components/article-list.js
+++ app/components/article-list.js
@@ -1,7 +1,11 @@
 import Component from '@ember/component';
+import { computed } from "@ember/object";
 
 export default Component.extend( {
   tagName: "",
+  hover: computed( '', () => {
+    
+  } ),
   actions: {
     goToPost( id ) {
       console.log( 'this is not right' + id );
