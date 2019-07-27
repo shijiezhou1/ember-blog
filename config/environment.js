311a3054d01c65480a8489128a8f60edf8e440ba
Shijie Zhou <cs5129606@gmail.com>
2019-07-27 08:41:51 -0600

    netlity branch

--- config/environment.js
+++ config/environment.js
@@ -47,7 +47,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.rootURL = '/Ember-Blog/';
+    ENV.rootURL = '/';
   }
 
   return ENV;
