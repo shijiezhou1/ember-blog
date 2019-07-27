6930c9566b97d3fdaf5bc41dbaec7f380f6198ba
Shijie Zhou <cs5129606@gmail.com>
2019-07-27 07:44:30 -0600

    making two array one is for odd and other for even so the content can display two different side
    making two array one is for odd and other for even so the content can display two different side

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,11 +1,28 @@
 import Route from '@ember/routing/route';
+import { A } from '@ember/array';
 
-export default Route.extend({
+export default Route.extend( {
   model() {
-    return this.store.findAll('post');
+    return this.store.findAll( 'post' );
   },
-  setupController: function (controller, model) {
-    controller.set('post', model);
+  /**
+   * Create even and odd posts structure
+   *
+   * @param controller
+   * @param model
+   */
+  setupController: function ( controller, model ) {
+    var evenArr = A( [] ),
+        oddArr = A( [] );
+    model.forEach( ( value, index ) => {
+      if ( ( index % 2 ) === 0 ) {
+        evenArr.push( value );
+      } else {
+        oddArr.push( value );
+      }
+    } );
+    controller.set( 'evenPost', evenArr );
+    controller.set( 'oddPost', oddArr );
   },
   // renderTemplate() {
   //   this.render('home', {
@@ -13,4 +30,4 @@ export default Route.extend({
   //     outlet: 'onlyApp'
   //   });
   // }
-});
+} );
--- mirage/fixtures/posts.js
+++ mirage/fixtures/posts.js
@@ -1,7 +1,7 @@
 export default [
   {
     "id": 1,
-    "title": "I'm Running to Reform the W3C's Tag",
+    "title": "1-I'm Running to Reform the W3C's Tag",
     "text": "Some TextSome TextSome The titles of Washed Out's breakthrough song and the first single from Paracosm share two most important words in Ernest Greene's musical language: feel it. It's a simple request, as wel It's a simple request, as well",
     "img": "https://res.cloudinary.com/practicaldev/image/fetch/s--o2WWhI0x--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/j1uy5mzlswvr45z7znjv.jpg",
     "url": "www.google.com",
@@ -11,7 +11,7 @@ export default [
   },
   {
     "id": 2,
-    "title": "Rails is omakase",
+    "title": "2-Rails is omakase",
     "text": "Other text",
     "img": "https://res.cloudinary.com/practicaldev/image/fetch/s--yuvBDNvQ--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://res.cloudinary.com/practicaldev/image/fetch/s--KDeQCWRL--/c_imagga_scale%2Cf_auto%2Cfl_progressive%2Ch_420%2Cq_auto%2Cw_1000/https://thepracticaldev.s3.amazonaws.com/i/fobvk7m7w88zk809a9b6.png",
     "url": "www.google.com",
@@ -21,7 +21,7 @@ export default [
   },
   {
     "id": 3,
-    "title": "Master infor",
+    "title": "3-Master infor",
     "text": "Other random text",
     "url": "www.google.com",
     "img": "https://res.cloudinary.com/practicaldev/image/fetch/s--yuvBDNvQ--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://res.cloudinary.com/practicaldev/image/fetch/s--KDeQCWRL--/c_imagga_scale%2Cf_auto%2Cfl_progressive%2Ch_420%2Cq_auto%2Cw_1000/https://thepracticaldev.s3.amazonaws.com/i/fobvk7m7w88zk809a9b6.png",
@@ -31,12 +31,22 @@ export default [
   },
   {
     "id": 4,
-    "title": "Four infor",
+    "title": "4-Four infor",
     "text": "Other okathadsjkasdk saj kjadkl jsdjadsad ad jasd askdjaskljdsakj dalskjd sakjd sakjd lkasjd kajs la;d text",
     "url": "www.google.com",
     "img": "https://res.cloudinary.com/practicaldev/image/fetch/s--yuvBDNvQ--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://res.cloudinary.com/practicaldev/image/fetch/s--KDeQCWRL--/c_imagga_scale%2Cf_auto%2Cfl_progressive%2Ch_420%2Cq_auto%2Cw_1000/https://thepracticaldev.s3.amazonaws.com/i/fobvk7m7w88zk809a9b6.png",
     "iconButtonList": {favorite: "favorite", share: "share"},
     "categoryList": [],
     "viewCount": 1
-  }
+  },
+  {
+    "id": 5,
+    "title": "5-I'm Running to Reform the W3C's Tag",
+    "text": "Some TextSome TextSome The titles of Washed Out's breakthrough song and the first single from Paracosm share two most important words in Ernest Greene's musical language: feel it. It's a simple request, as wel It's a simple request, as well",
+    "img": "https://res.cloudinary.com/practicaldev/image/fetch/s--o2WWhI0x--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/j1uy5mzlswvr45z7znjv.jpg",
+    "url": "www.google.com",
+    "iconButtonList": {favorite: "favorite", share: "share"},
+    "categoryList": [],
+    "viewCount": 2
+  },
 ]
