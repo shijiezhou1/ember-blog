d7374a98c5e9ec2bcc51e654ebd93f1a805ecf12
Shijie Zhou <cs5129606@gmail.com>
2019-07-28 19:26:51 -0600

    adding some note and remote unuse object

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,4 +1,6 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  // TODO two options for gallery list view: https://masonry.desandro.com/ or https://vestride.github.io/Shuffle/
+  // those two option is very useful
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -5,8 +5,6 @@ export default Route.extend({
   },
   setupController(controller, model) {
     this._super(...arguments);
-    // TODO thinking of way to wrap up the post under the post route instead on the application. The applicaiton is only for the general rendering stuff
-    // controller.set('post', this.store.findAll('post'));
     controller.set('category', this.store.findAll('category'));
   }
 });
