584eb4747bd40f12de8be696d30ad38e5a954e2a
Shijie Zhou <cs5129606@gmail.com>
2019-08-18 11:36:51 -0600

    no more faker

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -1,19 +1,17 @@
 // import { JSONAPISerializer } from 'ember-cli-mirage';
 // import { RestSerializer } from 'ember-cli-mirage';
-import { Serializer } from 'ember-cli-mirage';
-export default Serializer;
 
-// import { Serializer } from 'ember-cli-mirage';
-//
-// export default Serializer.extend({
-//   serialize({ models }, request) {
-//     let perPage = parseInt(request.queryParams.per_page, 10);
-//     let startPage = parseInt(request.queryParams.page, 10);
-//
-//     let pageCount = Math.ceil(models.length / perPage);
-//     let offset = perPage * (startPage - 1);
-//     let subset = models.slice(offset, offset + perPage);
-//
-//     return { posts: subset, meta: { total_pages: pageCount } };
-//   }
-// });
+import { Serializer } from 'ember-cli-mirage';
+
+export default Serializer.extend({
+  serialize({ models }, request) {
+    let perPage = parseInt(request.queryParams.per_page, 10);
+    let startPage = parseInt(request.queryParams.page, 10);
+
+    let pageCount = Math.ceil(models.length / perPage);
+    let offset = perPage * (startPage - 1);
+    let subset = models.slice(offset, offset + perPage);
+
+    return { posts: subset, meta: { total_pages: pageCount } };
+  }
+});
