50f04001b56e8c1fa4c5e68dcf35f59b125fec15
Shijie Zhou <cs5129606@gmail.com>
2019-11-14 20:17:02 -0700

    fixed some loading slow issue

--- app/routes/self.js
+++ app/routes/self.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return new Promise((resolve) => {
-			setTimeout(resolve, 3000);
+			setTimeout(resolve, 1000);
 		});
   }
 });
