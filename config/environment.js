736a9b1edfc85d259751e73b88c60de0bb1a5388
Shijie Zhou <cs5129606@gmail.com>
2020-05-21 00:07:11 +0800

    Environment variable

--- config/environment.js
+++ config/environment.js
@@ -51,7 +51,7 @@ module.exports = function(environment) {
 
   if (environment === "production") {
     // here you can enable a production-specific feature
-    ENV.rootURL = "/Ember-Blog/"
+    ENV.rootURL = "/ember-blog/"
   }
 
   return ENV
